f9dba743f520399274ebe56474531264d62f95a0
unknown <dorianekengni@gmail.com>
2025-02-24 16:41:57 +0200

    add AppointmentForm component

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './DoctorCard.css';
-import AppointmentFormIC from '../AppointmentFormIC/AppointmentFormIC'
+import AppointmentForm from '../AppointmentForm/AppointmentForm'
 import { v4 as uuidv4 } from 'uuid';
 
 
@@ -50,7 +50,7 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
       </div>
 
 
-      {/* <div className="doctor-card-options-container">
+      <div className="doctor-card-options-container">
        <Popup
           style={{ backgroundColor: '#FFFFFF' }}
           trigger={
@@ -93,12 +93,12 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
                   ))}
                 </>
               ) : (
-                <AppointmentFormIC doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} />
+                <AppointmentForm doctorName={name} doctorSpeciality={speciality} onSubmit={handleFormSubmit} />
               )}
             </div>
           )}
         </Popup> 
-      </div> */}
+      </div>
     </div>
   );
 };
