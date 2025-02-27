ae9ff04b7b7fc108c81f6787eb6e084b17209e24
kengnidoriane <dorianekengni@gmail.com>
2025-02-27 14:32:53 +0100

    fix: services component view

--- src/Components/Services/Service.js
+++ src/Components/Services/Service.js
@@ -4,27 +4,27 @@ import './Services.css'
 
 function Service() {
   return (
-    <div>
+    <div className='services'>
         <h2>Best Practices</h2>
         <p>Take care of your body. It's the only place you have to live</p>
         <div className='container-card-services'>
             <ServiceCard
-                imageUrl="https://example.com/image.jpg"
+                imageUrl="/images/instant-consultation.png"
                 title="Instant Consultation"
                 link="/instant-consultation"
             />
              <ServiceCard
-                imageUrl="https://example.com/image.jpg"
+                imageUrl="/images/instant-consultation.png"
                 title="Booking Appointment"
                 link="/booking-consultation"
             />
              <ServiceCard
-                imageUrl="https://example.com/image.jpg"
+                imageUrl="/images/instant-consultation.png"
                 title="Self Checkup"
                 link="/autre-page"
             />
              <ServiceCard
-                imageUrl="https://example.com/image.jpg"
+                imageUrl="/images/instant-consultation.png"
                 title="Health Tips and Guidance"
                 link="/autre-page"
             />
